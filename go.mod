cc0bdaa8a23294453942c11e759433b554efb176
emre-guler <emre_guler01@hotmail.com>
2022-08-01 03:05:05 +0300

    Progess for main, added Db file and Regex Validation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/emre-guler/url-shortener
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/emre-guler/url-shortener/db"
+	"github.com/emre-guler/url-shortener/validation"
+	"os"
+)
+
+const shortUrl string = "https://www.emreguler.dev/"
+
+func main() {
+	fmt.Println("Welcome to url-shortener!")
+	fmt.Println("---------------------")
+	fmt.Println("Enter the link you want to shorten: ")
+
+	readerObject := bufio.NewReader(os.Stdin)
+	guestLongUrl, _ := readerObject.ReadString('\n')
+	if validation.IsValidUrl(guestLongUrl) {
+		fmt.Println("Enter the path you want: ")
+		guestShortPath, _ := readerObject.ReadString('\n')
+		var currentUrl = shortUrl + guestShortPath
+		if validation.IsValidUrl(currentUrl) {
+			fmt.Println("Checking db for availability...")
+			if db.CheckShortPath(currentUrl) {
+				if db.SaveShortPath(currentUrl) {
+
+				} else {
+					fmt.Println("Something went wrong! Try again later.")
+				}
+			} else {
+				fmt.Println("This abbreviation is in use, please try another abbreviation.")
+			}
+		} else {
+			fmt.Println("Enter a valid URL.")
+		}
+	} else {
+		fmt.Println("Enter a valid URL.")
+	}
+}
