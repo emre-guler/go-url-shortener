3cc5136bbf2080bc45e23ae5635cb33eb274e8e5
emre-guler <emre_guler01@hotmail.com>
2022-08-01 08:43:08 +0300

    Fix regex

--- validation/validation.go
+++ validation/validation.go
@@ -2,7 +2,7 @@ package validation
 
 import "regexp"
 
-const regExpression string = "(\\b(https?|ftp|file)://)?[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]\n"
+const regExpression string = "(\b(https?|ftp|file)://)?[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]"
 
 func IsValidUrl(url string) bool {
 	isValid, _ := regexp.MatchString(regExpression, url)
