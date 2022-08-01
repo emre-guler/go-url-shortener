78faa6c54a47153044ce804287c6f1f55895c5d0
emre-guler <emre_guler01@hotmail.com>
2022-08-01 13:03:00 +0300

    Google API progress.

--- db/db.go
+++ db/db.go
@@ -1,10 +1,46 @@
 package db
 
-// TODO Use Google Sheets as DB.
+import (
+	"context"
+	"fmt"
 
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+)
+
+// TODO Use Google Sheets as DB.
 // TODO Use Redis for caching.
+const (
+	spreadsheetId = "SHEET_ID_HERE"
+	readRange     = "A:B"
+)
 
 func CheckShortPath(path string) bool {
+	ctx := context.Background()
+	service, serviceError := sheets.NewService(ctx, option.WithAPIKey("YOUR_API_KEY_HERE"))
+
+	if serviceError != nil {
+		fmt.Println("Unable to connect service, please try again later!1")
+		return false
+	}
+
+	response, responsError := service.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+
+	if responsError != nil {
+		fmt.Println("Unable to connect service, please try again later!2")
+		fmt.Println(responsError)
+		return false
+	}
+
+	if len(response.Values) == 0 {
+		fmt.Print("No data found.")
+		return false
+	} else {
+		for _, row := range response.Values {
+			fmt.Printf("%s, %s\n", row[0], row[1])
+		}
+	}
+
 	return false
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/emre-guler/url-shortener
 
 go 1.18
+
+require (
+	cloud.google.com/go/compute v1.7.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
+	golang.org/x/oauth2 v0.0.0-20220722155238-128564f6959c // indirect
+	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.90.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220728213248-dd149ef739b9 // indirect
+	google.golang.org/grpc v1.48.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
